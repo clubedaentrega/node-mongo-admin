17c366712b82e5a3fc00e987577a77595769249c
Guilherme Souza <sitegui@sitegui.com.br>
2016-10-15 21:28:37 -0300

    Draft to auto-complete engine

---
+++ auto-complete/parse.js
@@ -0,0 +1,286 @@
+'use strict'
+
+/**
+ * @typedef {Object} Parsed
+ * @property {string} type
+ * @property {string} raw - raw source string
+ */
+
+/**
+ * @typedef {Parsed} ParsedObject
+ * @property {Array<ParsedKeyValue>} properties
+ * @property {number} cursor - property index the cursor is at
+ */
+
+/**
+ * @typedef {Parsed} ParsedKeyValue
+ * @property {string} key
+ * @property {Parsed} value
+ * @property {number} keyCursor - cursor position in key text
+ */
+
+/**
+ * @typedef {Parsed} ParsedArray
+ * @property {Array<Parsed>} values
+ * @property {number} cursor - value index the cursor is at
+ */
+
+/**
+ * @typedef {Parsed} ParsedSource
+ * @property {number} cursor - cursor position in raw text
+ */
+
+/**
+ * Extract a JS expression from the beginning of the string.
+ * It uses delimiter couting to know where the expression ends.
+ * It is not an error to leave delimiter unclosed, like in "{a: 2".
+ * Unmatched closing delimiters are ignore, like ')' in "{a: )}"
+ * @param {ParsedSource} source - will be modified
+ * @returns {ParsedSource}
+ */
+function readValue(source) {
+	// Stack of open delimiters: {, [, (, `, ', "
+	let stack = [],
+		// Last stack level
+		mode = 'root',
+		// Whether this is may be a pure object or array
+		seemsPure = true,
+		// First delimiter
+		first = '',
+		i
+
+	for (i = 0; i < source.raw.length; i++) {
+		let c = source.raw[i]
+
+		if ((mode === '{' && c === '}') ||
+			(mode === '[' && c === ']') ||
+			(mode === '(' && c === ')') ||
+			(mode === '`' && c === '`') ||
+			(mode === '\'' && c === '\'') ||
+			(mode === '"' && c === '"')) {
+			// Close delimiter
+			mode = stack.pop()
+		} else if (c === '{' || c === '[' || c === '(' ||
+			c === '`' || c === '\'' || c === '"') {
+			// Open delimiter
+			stack.push(mode)
+			mode = c
+
+			if (!first) {
+				first = c
+			}
+		} else if ((mode === '`' || mode === '\'' || mode === '"') && c === '\\') {
+			// Escape next char
+			i += 1
+		} else if (mode === 'root' && c === ',') {
+			// End of expression
+			break
+		} else if (seemsPure && mode === 'root' && !/^\s$/.test(c)) {
+			// Pure objects/arrays can't have any non-whitespace char at root
+			seemsPure = false
+		}
+	}
+
+	// Slice source
+	let valueSource = {
+		raw: source.raw.slice(0, i + 1),
+		cursor: sliceCursor(source.cursor, 0, i + 1)
+	}
+	source.cursor = sliceCursor(source.cursor, i + 1, source.raw.length)
+	source.raw = source.raw.slice(i + 1)
+
+	// Promote pure types
+	if (seemsPure && first === '{') {
+		return promoteObject(valueSource, mode === 'root')
+	} else if (seemsPure && first === '[') {
+		return promoteArray(valueSource, mode === 'root')
+	}
+
+	// Fallback to raw source
+	return valueSource
+}
+
+/**
+ * Parse an object source
+ * @param {ParsedSource} source
+ * @param {boolean} gentleEnd - whether the object body was ended correctly
+ * @returns {ParsedObject}
+ */
+function promoteObject(source, gentleEnd) {
+	// Extract object body string
+	// The raw string is garanteed to start with spaces and a '{'
+	// Depending on gentleEnd it may end with or without '}' spaces and ','
+	let match = source.raw.match(gentleEnd ? /^(\s*\{)(.*)\}\s*,?$/ : /^(\s*\{)(.*)$/),
+		before = match[1],
+		body = match[2],
+		subSource = {
+			type: 'source',
+			raw: body,
+			cursor: sliceCursor(source.cursor, before.length, before.length + body.length)
+		}
+
+	// Read properties
+	let properties = [],
+		cursor = -1,
+		key
+	while (/\S/.test(subSource.raw)) {
+		let hadCursor = subSource.cursor !== -1
+		key = readKey(subSource)
+		properties.push({
+			key: key.name,
+			value: readValue(subSource),
+			keyCursor: key.cursor
+		})
+		if (hadCursor && subSource.cursor === -1) {
+			// Cursor is trapped in the property
+			cursor = properties.length - 1
+		}
+	}
+
+	if (subSource.cursor !== -1) {
+		// Cursor at the end
+		cursor = properties.length
+	}
+
+	return {
+		type: 'object',
+		raw: source.raw,
+		properties: properties,
+		cursor: cursor
+	}
+}
+
+/**
+ * Parse an array source
+ * @param {ParsedSource} source
+ * @param {boolean} gentleEnd - whether the array body was ended correctly
+ * @returns {ParsedArray}
+ */
+function promoteArray(source, gentleEnd) {
+	// Extract array body string
+	// The raw string is garanteed to start with spaces and a '['
+	// Depending on gentleEnd it may end with or without ']' spaces and ','
+	let match = source.raw.match(gentleEnd ? /^(\s*\[)(.*)\]\s*,?$/ : /^(\s*\[)(.*)$/),
+		before = match[1],
+		body = match[2],
+		subSource = {
+			type: 'source',
+			raw: body,
+			cursor: sliceCursor(source.cursor, before.length, before.length + body.length)
+		}
+
+	// Read values
+	let values = [],
+		cursor = -1
+	while (/\S/.test(subSource.raw)) {
+		let hadCursor = subSource.cursor !== -1
+		values.push(readValue(subSource))
+		if (hadCursor && subSource.cursor === -1) {
+			// Cursor is trapped in the property
+			cursor = values.length - 1
+		}
+	}
+
+	if (subSource.cursor !== -1) {
+		// Cursor at the end
+		cursor = values.length
+	}
+
+	return {
+		type: 'array',
+		raw: source.raw,
+		values: values,
+		cursor: cursor
+	}
+}
+
+/**
+ * Extract an object key from the beginning of the string.
+ * It is not an error to not use quotes when needed, like: "a.b: 2".
+ * Invalid characters between close quotes and ':' are ignore, like 'x' in '"a"x: 2'
+ * @param {ParsedSource} source - will be modified
+ * @returns {?{name: string, cursor: number}}
+ */
+function readKey(source) {
+	let mode = 'start',
+		// First char in the key
+		startIndex = -1,
+		// First char not in the key
+		endIndex = -1,
+		i
+
+	for (i = 0; i < source.raw.length; i++) {
+		let c = source.raw[i]
+
+		if (mode === 'start') {
+			if (/^\s$/.test(c)) {
+				// Ignore leading spaces
+			} else if (c === '\'' || c === '"') {
+				// Quoted key
+				mode = c
+				startIndex = i + 1
+			} else {
+				// Unquoted
+				mode = 'bare'
+				startIndex = i
+			}
+		} else if (mode === '\'' || mode === '"') {
+			if (c === '\\') {
+				// Escape next char
+				i += 1
+			} else if (c === mode) {
+				// End of key
+				mode = 'end'
+				endIndex = i
+			}
+		} else if (mode === 'bare') {
+			if (c === ':') {
+				// End unquoted key
+				endIndex = i
+				break
+			}
+		} else if (mode === 'end') {
+			if (c === ':') {
+				// End quoted key
+				break
+			}
+		}
+	}
+
+	if (startIndex === -1) {
+		// Not found
+		return null
+	}
+
+	if (endIndex === -1) {
+		// Assume the whole text is part of the key
+		endIndex = source.raw.length
+	}
+
+	let key = {
+		name: source.raw.slice(startIndex, endIndex),
+		cursor: sliceCursor(source.cursor, startIndex, endIndex)
+	}
+	source.cursor = sliceCursor(source.cursor, i + 1, source.raw.length)
+	source.raw = source.raw.slice(i + 1)
+	return key
+}
+
+/**
+ * Compute the new position for the cursor after a slice
+ * @param {number} cursor
+ * @param {number} start
+ * @param {number} end
+ */
+function sliceCursor(cursor, start, end) {
+	if (cursor === -1) {
+		// Already out of bounds
+		return -1
+	} else if (cursor >= start && cursor <= end) {
+		// Moved
+		return cursor - start
+	} else {
+		// Will be out of bounds
+		return -1
+	}
+}
---
+++ auto-complete/sample.js
@@ -0,0 +1,193 @@
+'use strict'
+
+if (process.argv.length < 4) {
+	console.log('Usage: node sample <connection> <collection>')
+	process.exit()
+}
+
+/**
+ * @typedef {Object} SchemaNode
+ * @property {?boolean|Array<number>} double - up to 10 values
+ * @property {?boolean|Array<string>} string - up to 10 values
+ * @property {?boolean} object
+ * @property {?boolean} array
+ * @property {?boolean} binData
+ * @property {?boolean} objectId
+ * @property {?boolean} bool
+ * @property {?boolean} date
+ * @property {?boolean} null
+ * @property {?boolean} regex
+ * @property {?boolean} timestamp
+ * @property {?boolean} long
+ * @property {?boolean} minKey
+ * @property {?boolean} maxKey
+ * @property {?Object<SchemaNode>} children - children by field name
+ */
+
+/** @var {SchemaNode} */
+let schema = {},
+	mongodb = require('mongodb')
+
+require('../dbs')((err, dbs) => {
+	if (err) {
+		throw err
+	}
+
+	let db = dbs[process.argv[2]]
+
+	if (!db) {
+		throw new Error('Invalid db')
+	}
+
+	console.log('Sample collection')
+	db.collection(process.argv[3]).aggregate([{
+		$sample: {
+			size: 100
+		}
+	}]).each((err, doc) => {
+		if (err) {
+			throw err
+		} else if (!doc) {
+			console.log(JSON.stringify(schema, null, '\t'))
+			process.exit()
+		}
+
+		processObj(doc, schema)
+	})
+})
+
+/**
+ * Process a type 'object' value
+ * @param {Object} doc
+ * @param {SchemaNode} schema
+ */
+function processObj(obj, schema) {
+	let hadChildren = true
+	schema.object = true
+
+	// Look for missing fields
+	if (!schema.children) {
+		schema.children = Object.create(null)
+		hadChildren = false
+	} else {
+		for (let key in schema.children) {
+			if (!(key in obj)) {
+				schema.children[key].null = true
+			}
+		}
+	}
+
+	// Walk fields
+	for (let key in obj) {
+		let childSchema = schema.children[key]
+
+		if (!childSchema) {
+			childSchema = schema.children[key] = !hadChildren ? {} : {
+				null: true
+			}
+		}
+
+		processValue(obj[key], childSchema)
+	}
+}
+
+/**
+ * Process a type 'array' value
+ * @param {Array<*>} arr
+ * @param {SchemaNode} schema
+ */
+function processArray(arr, schema) {
+	let hadChildren = true
+	schema.array = true
+
+	// Look for missing fields
+	if (!schema.children) {
+		schema.children = Object.create(null)
+		hadChildren = false
+	} else {
+		for (let key in schema.children) {
+			if (key !== '$') {
+				schema.children[key].null = true
+			}
+		}
+	}
+
+	let childSchema = schema.children.$
+
+	if (!childSchema) {
+		childSchema = schema.children.$ = !hadChildren ? {} : {
+			null: true
+		}
+	}
+
+	// Walk values
+	arr.forEach(element => {
+		processValue(element, childSchema)
+	})
+}
+
+/**
+ * @param {*} value
+ * @param {SchemaNode} schema
+ */
+function processValue(value, schema) {
+	if (typeof value === 'number') {
+		addToEnum(schema, 'double', value)
+	} else if (typeof value === 'string') {
+		addToEnum(schema, 'string', value)
+	} else if (value === null) {
+		schema.null = true
+	} else if (value instanceof Date) {
+		schema.date = true
+	} else if (value instanceof RegExp) {
+		schema.regex = true
+	} else if (typeof value === 'boolean') {
+		schema.bool = true
+	} else if (value instanceof mongodb.ObjectId) {
+		schema.objectId = true
+	} else if (value instanceof mongodb.Binary) {
+		schema.binData = true
+	} else if (value instanceof mongodb.Timestamp) {
+		schema.timestamp = true
+	} else if (value instanceof mongodb.Long) {
+		schema.long = true
+	} else if (value instanceof mongodb.MinKey) {
+		schema.minKey = true
+	} else if (value instanceof mongodb.MaxKey) {
+		schema.maxKey = true
+	} else if (Array.isArray(value)) {
+		processArray(value, schema)
+	} else if (typeof value === 'object' &&
+		Object.getPrototypeOf(value) === Object.prototype) {
+		processObj(value, schema)
+	}
+}
+
+/**
+ * Handle value enumerations (up to 10)
+ * @param {SchemaNode} schema - modified in-place
+ * @param {string} typeName
+ * @param {number|string} value
+ */
+function addToEnum(schema, typeName, value) {
+	let type = schema[typeName]
+
+	if (type === true) {
+		// No longer an enum
+		return
+	} else if (typeof value === 'string' && value.length > 50) {
+		// Too large
+		schema[typeName] = true
+		return
+	} else if (!type) {
+		type = schema[typeName] = []
+	}
+
+	if (type.indexOf(value) === -1) {
+		type.push(value)
+		if (type.length > 10) {
+			// Too many values
+			schema[typeName] = true
+		}
+	}
+}
